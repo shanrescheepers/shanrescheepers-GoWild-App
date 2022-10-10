ca313f45e3903246d9964b6158fb842d470c702a
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-10-10 20:25:41 +0200

    FT:  git init
    App Init

---
+++ Leopards-of-the-Greater-Kruger/Leopards-of-the-Greater-Kruger/Leopards_of_the_Greater_KrugerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leopards_of_the_Greater_KrugerApp.swift
+//  Leopards-of-the-Greater-Kruger
+//
+//  Created by Mac on 2022/10/10.
+//
+
+import SwiftUI
+
+@main
+struct Leopards_of_the_Greater_KrugerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
